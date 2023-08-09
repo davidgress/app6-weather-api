bd01ba674072a22ef4b3e0f943311b1a4bc068ac
davidgress <david.e.gress@gmail.com>
2023-08-09 08:28:57 -0400

    Day 31: Add url endpoint for yearly and all data.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 import pandas as pd
 
-
 app = Flask(__name__)
 
 variable = "Hello There"
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[['STAID', 'STANAME                                 ']]
+
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
@@ -14,12 +15,30 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    filename= "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
 
 
 if __name__ == "__main__":
